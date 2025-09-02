426c159a6656cc0a1d407ce61a5b857da527b20f
邱榆文 <chiuyuwen@qiuyuwendeMacBook-Air.local>
2025-09-03 05:46:52 +0800

    Fix typo

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def time_difference_analysis():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port, , debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
