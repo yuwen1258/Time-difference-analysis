65495cfd57f466de1e7af6007cd15249f1a83672
邱榆文 <chiuyuwen@qiuyuwendeMacBook-Air.local>
2025-09-03 03:07:02 +0800

    modified some web function

--- app.py
+++ app.py
@@ -21,15 +21,27 @@ def time_difference_analysis():
             return '', 204
         
         data = request.get_json()
+        print("從前端取得資料")
         if not data:
             return jsonify({"error": "請求中未包含 JSON 資料"}), 400
+
+        # 比較圖層時間新舊
+        layer1_info = data.get('itemId1')
+        layer2_info = data.get('itemId2')
+        if not layer1_info or not layer2_info:
+            return jsonify({"error": "請求中缺少 itemId1 或 itemId2 參數"}), 400
+
+        if layer1_info['year'] > layer2_info['year']:
+            newer_layer_info = layer1_info
+            older_layer_info = layer2_info
+        else:
+            newer_layer_info = layer2_info
+            older_layer_info = layer1_info
+        print("完成時間比較")
         
         # 從 JSON 中取得前端傳來的兩個 Item ID
-        item_id_1 = data.get('itemId1')
-        item_id_2 = data.get('itemId2')
-
-        if not item_id_1 or not item_id_2:
-            return jsonify({"error": "請求中缺少 itemId1 或 itemId2 參數"}), 400
+        item_newer_id = newer_layer_info['id']
+        item_older_id = older_layer_info['id']
 
         # 1. 從環境變數讀取 ArcGIS Online 的帳號和密碼
         username = os.getenv("ARCGIS_USERNAME")
@@ -42,51 +54,47 @@ def time_difference_analysis():
         gis = GIS("https://igisportal.geomatics.ncku.edu.tw/portal/", username=username, password=password)
         print(f"成功以使用者 '{username}' 的身份登入 ArcGIS Online...")
 
-
         # 3. 透過 Item ID 取得圖層
+        item_newer = gis.content.get(item_newer_id)
+        item_older = gis.content.get(item_older_id)
 
-        item_1 = gis.content.get(item_id_1)
-        item_2 = gis.content.get(item_id_2)
-
-        layer_1 = item_1.layers[0]
-        layer_2 = item_2.layers[0]
+        layer_newer = item_newer.layers[0]
+        layer_older = item_older.layers[0]
 
         # 4. 查詢圖層資料並轉換為 Spatially Enabled DataFrame
-        sdf_1 = layer_1.query().df
-        sdf_2 = layer_2.query().df
+        sdf_1 = layer_newer.query().df
+        sdf_2 = layer_older.query().df
         print("成功從 ArcGIS Online 取得圖層資料...")
 
         # 5. 執行分析
-        #sdf_1 = sdf_1.rename(columns={'p_cnt': 'POP_1', 'info_time': 'year1'})
-        #sdf_2 = sdf_2.rename(columns={'p_cnt': 'POP_2', 'info_time': 'year2'})
-        sdf_1 = sdf_1.rename(columns={'info_time': 'year1'})
-        sdf_2 = sdf_2.rename(columns={'info_time': 'year2'})
+        # 取得計算欄位名稱
+        newer_field_name = newer_layer_info['field']
+        older_field_name = older_layer_info['field']
+        print(newer_field_name)
+        # 取得地理空間欄位名稱
+        LID_name = newer_layer_info['location_id']
+        print(LID_name)
 
-        if int(sdf_1['year1'][0].split('Y')[0]) > int(sdf_2['year2'][0].split('Y')[0]):
-            newer_layer = sdf_1
-            older_layer = sdf_2
+        # 查詢資料
+        sdf_newer = item_newer.layers[0].query().df
+        sdf_older = item_older.layers[0].query().df
 
-            newer_layer = newer_layer.rename(columns={'p_cnt': 'POP_newer', 'year1': 'year_newer'})
-            older_layer = older_layer.rename(columns={'p_cnt': 'POP_older', 'year2': 'year_older'})
-        else:
-            newer_layer = sdf_2
-            older_layer = sdf_1
+        # 重新命名欄位
+        sdf_newer = sdf_newer.rename(columns={
+            LID_name: 'LID', newer_field_name: 'VALUE_NEW', 'info_time': 'YEAR_NEW'})
+        sdf_older = sdf_older.rename(columns={
+            LID_name: 'LID', older_field_name: 'VALUE_OLD', 'info_time': 'YEAR_OLD'})
 
-            newer_layer = newer_layer.rename(columns={'p_cnt': 'POP_newer', 'year2': 'year_newer'})
-            older_layer = older_layer.rename(columns={'p_cnt': 'POP_older', 'year1': 'year_older'})
-        print("時間判斷成功...")
-        
-        newer_simple = newer_layer[['villcode', 'village', 'POP_newer', 'year_newer', 'SHAPE']]
-        older_simple = older_layer[['villcode', 'village', 'POP_older', 'year_older']]
-        
-        diff_sdf = newer_simple
-        diff_sdf['year_older'] = older_simple['year_older']
-        diff_sdf['POP_older'] = older_simple['POP_older']
-        diff_sdf['POP_DIFF'] = newer_simple['POP_newer'] - older_simple['POP_older']
+        # 準備合併用的資料
+        sdf_newer_simple = sdf_newer[['LID', 'VALUE_NEW', 'YEAR_NEW', 'SHAPE']]
+        sdf_older_simple = sdf_older[['LID', 'VALUE_OLD', 'YEAR_OLD']]
+
+        merged_df = sdf_newer_simple.merge(sdf_older_simple, on='LID')
+        merged_df['VALUE_DIFF'] = merged_df['VALUE_NEW'] - merged_df['VALUE_OLD']
         print("資料分析運算完成...")
 
         # 6. 將結果轉換為 GeoJSON 並回傳
-        result_geojson = diff_sdf.to_json(orient="records")
+        result_geojson = merged_df.to_json(orient="records")
         
         return Response(result_geojson, mimetype='application/json')
 
