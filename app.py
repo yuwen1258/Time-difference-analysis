e8b2979988e652f779ad36cab57d5fdc24cf5389
邱榆文 <chiuyuwen@qiuyuwendeMacBook-Air.local>
2025-09-03 05:34:54 +0800

    Final Debug: Bypass Gunicorn and run app.py directly

--- app.py
+++ app.py
@@ -103,5 +103,5 @@ def time_difference_analysis():
         return response, 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, , debug=True)
